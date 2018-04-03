2260cdd30279edd1a28d2b7b82fa58259ec77a87
marcogonca96 <marcogonca1996@gmail.com>
2018-04-04 00:31:41 +0100

    Merge branch 'USP03---Consult-activity-plan' of https://github.com/cubicon-feup/prehab-mobile into USP03---Consult-activity-plan

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { Dimensions } from 'react-native';
+
+export class HomeScreen extends React.Component {
+  render() {
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <TouchableOpacity
+                    key={"exercise"}
+                    style={styles.item}
+                    onPress={() => this.onExercise()}
+                >
+                    <Image
+                        style={styles.itemIcon}
+                        source={require('../assets/img/cardiogram.png')}
+                    />
+                    <Text style={styles.itemTitle}>
+                        {"Exercício"}
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    key={"nutrition"}
+                    style={styles.item}
+                    onPress={() => this.onNutrition()}
+                >
+                    <Image
+                        style={styles.itemIcon}
+                        source={require('../assets/img/restaurant.png')}
+                    />
+                    <Text style={styles.itemTitle}>
+                        {"Nutrição"}
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    key={"alerts"}
+                    style={styles.item}
+                    //onPress={() => this.onExercise()}
+                >
+                    <Image
+                        style={styles.itemIcon}
+                        source={require('../assets/img/alarm.png')}
+                    />
+                    <Text style={styles.itemTitle}>
+                        {"Alertas"}
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    key={"help"}
+                    style={styles.item}
+                    //onPress={() => this.onExercise()}
+                >
+                    <Image
+                        style={styles.itemIcon}
+                        source={require('../assets/img/ambulance.png')}
+                    />
+                    <Text style={styles.itemTitle}>
+                        {"Ajuda"}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    );
+  }
+
+  onExercise = () => {
+      this.props.navigation.navigate('ExerciseScreen');
+  }
+
+  onNutrition = () => {
+      this.props.navigation.navigate('NutritionScreen');
+  }
+}
+
+var styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    item: {
+        width: Dimensions.get('window').width * 0.5,
+        height: 300,
+        borderWidth: 1,
+        borderColor: "lightgray",
+        alignItems: 'center',
+        justifyContent: 'center'        
+    },
+    itemIcon: {
+        width: 150,
+        height: 150,
+        resizeMode: 'contain'
+    },
+    itemTitle: {
+        marginTop: 16,
+        fontSize: 30
+    },
+});
